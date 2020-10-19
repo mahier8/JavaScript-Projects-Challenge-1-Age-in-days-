7559eeee89a844b558175cc4a9761926223106ee
Mahier <mahier.sydow8@gmail.com>
2020-10-19 19:54:45 +0900

    Second commit

--- static/script.js
+++ static/script.js
@@ -2,7 +2,7 @@
 
 function ageInDays() {
     var birthYear = prompt('What year were you born... good friend?');
-    var ageInDayss = (2018 - birthYear) * 356;
+    var ageInDayss = (2020 - birthYear) * 356;
     var h1 = document.createElement('h1');
     var textAnswer = document.createTextNode('You are ' + ageInDayss + ' days old.');
     h1.setAttribute('id', 'ageInDays');
@@ -12,4 +12,13 @@ function ageInDays() {
 
 function reset() {
     document.getElementById('ageInDays').remove();
+}
+
+// Challenge 2
+
+function generateCat() {
+    var image = document.createElement('img');
+    var div = document.getElementById('flex-cat-gen');
+    image.src = "https://thecatapi.com/api/images/get?format=src&type=gif&size=small";
+    div.appendChild(image);
 }
